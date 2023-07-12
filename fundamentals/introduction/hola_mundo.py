c5d210d6e952ea61bd09ff5ed4098ef363fb0951
Jonathan Didier Gimenez Gonzalez <jdg24g@gmail.com>
2023-07-12 18:26:45 -0400

    hola mundo

---
+++ fundamentals/introduction/hola_mundo.py
@@ -0,0 +1,12 @@
+nombre = "Didier"
+comida_uno = "Pizza"
+comida_dos = "Hamburguesa"
+num = "24"
+
+
+print("Hola Mundo")
+print("Hola", nombre,"!")
+print("Hola "+nombre+"!")
+print("!Hola "+num+"!")
+print("Amo comer {} y {}.".format(comida_uno, comida_dos))
+print(f"Amo comer {comida_uno} y {comida_dos}")
